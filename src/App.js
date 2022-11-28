dfaf4221d55ac85e6374599de505fb7cca7112a1
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-11-27 18:30:08 -0800

    apply styles for theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/system';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
 
 //* COMPONENTS
 import { UserSignIn } from './components/auth/UserSignIn';
 
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#EE3B55',
+      light: '#03a9f4'
+    },
+    secondary:{
+      main: '#FFFEFE'
+    },
+  },
+});
+
 function App() {
   return (
-    <Container disableGutters={true}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<UserSignIn />}/>
-        </Routes>
-      </Router>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container disableGutters={true}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<UserSignIn />}/>
+          </Routes>
+        </Router>
+      </Container>
+    </ThemeProvider>
   );
 }
 
