1b94d3687b459aa2f244f80cd03fdfc4ce780a16
gurb <Organicmintz@gmail.com>
2022-11-29 18:13:56 -0800

    add link to take in object

--- src/components/widgets/ThirdPartyAccountOptions.js
+++ src/components/widgets/ThirdPartyAccountOptions.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 
 /**
- * This function takes in 3 strings  and one object as parameters (icon(obj), name, link, and type). 
+ * This function takes in 2 strings  and 2 object as parameters (icon(obj), name, link(obj), and type). 
  *  -The icon(obj) is the img you would like to display. When calling in this function the img must be passed in as an object. 
  *  -The name is the what you would like the name of the 3rd party option to be displayed as
- *  -The link is where you would like the button to be linked
+ *  -The link(obj) is the function that will handle your 3rd party login
  *  -The type asks what type of btn you would like this to be. This is be displayed to the user
  * 
  * //* EXAMPLE in Code
  *  import exampleLogo from '../imgs/example-logo.png'
  * 
- *  <ThirdPartyAccountOptions icon={exampleLogo} name="Example" link="/examplePage" type="Sign in"/>
+ *  <ThirdPartyAccountOptions icon={exampleLogo} name="Example" link={signInWithGoogle} type="Sign in"/>
  * 
  * //* EXAMPLE on Screen
  *  ----------------------------------
@@ -25,7 +25,7 @@ import { Link } from 'react-router-dom';
 
 export function ThirdPartyAccountOptions({icon,name,link,type}){
     return(
-        <Link to={link} className='text-decoration-none text-dark'>
+        <Link to='#' onClick={link} className='text-decoration-none text-dark'>
             <div className="card m-3" style={{ height: '59.2px', width: '290px'}}>
                 <div className="row g-0 d-flex align-items-center mt-1">
                     <div className="col-4 mb-1 d-flex justify-content-evenly">
