317f8039608bce62a65a12d7c88d05dbd3b4b0be
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-12-04 17:26:54 -0800

    add route for reset password

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { PhoneSignUp } from './components/pages/PhoneSignUp';
 import { Footer } from './components/widgets/Footer';
 import { MainPage } from './components/pages/MainPage';
 import { PhoneSignIn } from './components/pages/PhoneSignIn';
+import { ResetPassword } from './components/pages/ResetPassword';
 
 
 
@@ -50,6 +51,7 @@ function App() {
             <Route path='/signup' element={<UserSignUp />}/>
             <Route path='/phonesignup' element={<PhoneSignUp />}/>
             <Route path='/phonesignin' element={<PhoneSignIn />}/>
+            <Route path='/resetpassword' element={<ResetPassword />}/>
           </Routes>
         </Router>
         <Footer sx={{ mt: 8, mb: 4 }} />
