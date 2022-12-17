d467e8fb710af3d609d928224dcedac4d97423ea
gurb <Organicmintz@gmail.com>
2022-12-16 21:14:25 -0800

    test privateroute

--- src/context/PrivateRoute.js
+++ src/context/PrivateRoute.js
@@ -1,18 +1,13 @@
 import React, { useContext } from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import SiteContext from './Context';
 
 export const PrivateRoute = ({ children }) => {
-    //import from router dom
-    const location = useLocation();
-
     // import from context api
     const { auth } = useContext(SiteContext)
-
-    // const auth = getAuth();
-   // const user = auth.currentUser;
-   console.log(auth.currentUser)
-
-    return auth.currentUser ? children : <Navigate to='/' replace state={{ from: location }}/>
-
+    console.log(auth.currentUser)
+    console.log(auth.displayName)
+    if(!auth){
+        return <Navigate to='/signin'/>
+    }
 }
