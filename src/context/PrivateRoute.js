9c2b6229bb5825fd7ec63bc87f273ed4e46dbe63
gurb <Organicmintz@gmail.com>
2023-01-05 15:29:20 -0800

    check if user is logged in

--- src/context/PrivateRoute.js
+++ src/context/PrivateRoute.js
@@ -4,10 +4,10 @@ import SiteContext from './Context';
 
 export const PrivateRoute = ({ children }) => {
     // import from context api
-    const { auth } = useContext(SiteContext)
-    console.log(auth.currentUser)
-    console.log(auth.displayName)
-    if(!auth){
+    const { currentUser } = useContext(SiteContext)
+    //if there is no user redirect to signin page if there is a cuser return the current page
+    if(!currentUser){
         return <Navigate to='/signin'/>
     }
+    return children
 }
