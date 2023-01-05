5f7f0df15930937c11359a6756341529a87061cf
gurb <Organicmintz@gmail.com>
2023-01-05 15:27:24 -0800

    add route for private route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import React, {useContext} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/system';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css'
@@ -16,7 +16,6 @@ import { MainPage } from './components/pages/MainPage';
 import { PhoneSignIn } from './components/pages/PhoneSignIn';
 import { ResetPassword } from './components/pages/ResetPassword';
 import { PrivateRoute } from './context/PrivateRoute';
-
 
 const font = "'Inder', sans-serif";
 
@@ -42,23 +41,21 @@ function App() {
 
   return (
     <SiteProvider>
-    <ThemeProvider theme={theme}>
-      <Container disableGutters={true}>
-        <Router>
-          <Routes>
-            <Route path='/' element={<PrivateRoute><MainPage /></PrivateRoute>}/>
-            <Route path="/signin" element={<UserSignIn />}/>
-            {/* <Route path='/' element={<UserSignIn />}/> */}
-            <Route path='/home' element={<MainPage />}/>
-            <Route path='/signup' element={<UserSignUp />}/>
-            <Route path='/phonesignup' element={<PhoneSignUp />}/>
-            <Route path='/phonesignin' element={<PhoneSignIn />}/>
-            <Route path='/resetpassword' element={<ResetPassword />}/>
-          </Routes>
-        </Router>
-        <Footer sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Container disableGutters={true}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<PrivateRoute><MainPage /></PrivateRoute>}/> 
+              <Route path="/signin" element={<UserSignIn />}/> 
+              <Route path='/signup' element={<UserSignUp />}/>
+              <Route path='/phonesignup' element={<PhoneSignUp />}/>
+              <Route path='/phonesignin' element={<PhoneSignIn />}/>
+              <Route path='/resetpassword' element={<ResetPassword />}/>
+            </Routes>
+          </Router>
+          <Footer sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
     </SiteProvider>
   );
 }
