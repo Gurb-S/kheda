4158845ceda1e62f289c5db1ad1afa1065948b61
gurb <Organicmintz@gmail.com>
2023-01-05 20:12:18 -0800

    add sign up error handling for email

--- src/components/pages/UserSignUp.js
+++ src/components/pages/UserSignUp.js
@@ -27,11 +27,21 @@ import { ref, uploadString } from "firebase/storage";
 import { doc, setDoc } from 'firebase/firestore';
 
 
+//* bullet point
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
 export function UserSignUp() {
 
   const navigate = useNavigate();
 
-  const [err, setErr ] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
 
   const handleSubmit = async (event) => {
 
@@ -49,7 +59,17 @@ export function UserSignUp() {
     //* END value provided by user
 
     const register = async () => {
+
+      console.log(userDisplayName)
+
       try{
+
+        if(!userDisplayName){
+          throw new Error('No username')
+        }
+        if(!userPassword){
+          throw new Error('Firebase: Password should be at least 6 characters (auth/weak-password).')
+        }
         //creates the user's account
         const res = await createUserWithEmailAndPassword(auth, userEmail, userPassword)
         //creates a reference to the user in firebase storage
@@ -75,9 +95,31 @@ export function UserSignUp() {
         console.log(res)
       } catch(error){
         console.log(error.message)
-        setErr(true)
+        if(error.message === 'Firebase: Error (auth/email-already-in-use).'){
+            setSignUpError('this email is already taken')
+            //console.log('Email is already used')
+        }
+        else if(error.message === 'Firebase: Error (auth/invalid-email).' || error.message === 'Firebase: Error (auth/missing-email).'){
+            setSignUpError('please provide a valid email')
+            //console.log('Not a valid email')
+        }
+        else if(error.message === 'No username'){
+          setSignUpError('please provide a username')
+          //console.log('Password needs to be atleast 6 characters')
+        }
+        else if(error.message === 'Firebase: Password should be at least 6 characters (auth/weak-password).'){
+            setSignUpError('password should be at least 6 characters')
+            //console.log('Password needs to be atleast 6 characters')
+        }
+        else{
+            setSignUpError('server error - please close app and try again')
+            //console.log('500 - Internal Server Error: Please close app and try again')
+        }
+        console.log('Sign Up Failed')
       }
     }
+
+    //Firebase Storage: The operation 'uploadString' cannot be performed on a root reference, create a non-root reference using child, such as .child('file.png'). (storage/invalid-root-operation)
     //! NEED TO DELETE
     console.log({
       username: data.get('username'),
@@ -116,7 +158,7 @@ export function UserSignUp() {
                   label="Username"
                   autoFocus
                   variant='filled'
-                  sx={{ bgcolor: 'secondary.main', borderRadius: '5px' }}
+                  sx={{ bgcolor: (signUpError.length >= 1 && signUpError === 'please provide a username' ? 'secondary.dark' : 'secondary.main'), borderRadius: '5px' }}
                 />
               </Grid>
               <Grid item xs={12} sx={{ mb: 1 }}>
@@ -128,7 +170,7 @@ export function UserSignUp() {
                   name="email"
                   autoComplete="email"
                   variant='filled'
-                  sx={{ bgcolor: 'secondary.main', borderRadius: '5px' }}
+                  sx={{ bgcolor: (signUpError.length >= 1 && signUpError !== 'password should be at least 6 characters' && signUpError !== 'please provide a username' ? 'secondary.dark' : 'secondary.main'), borderRadius: '5px' }}
                 />
               </Grid>
               <Grid item xs={12} sx={{ mb: 1 }}>
@@ -141,12 +183,20 @@ export function UserSignUp() {
                   id="password"
                   autoComplete="new-password"
                   variant='filled'
-                  sx={{ bgcolor: 'secondary.main', borderRadius: '5px' }}
+                  sx={{ bgcolor: (signUpError.length >= 1 && signUpError === 'password should be at least 6 characters' ||
+                  signUpError.length >= 1 && signUpError === 'server error - please close app and try again' ? 'secondary.dark' : 'secondary.main'), borderRadius: '5px' }}
                 />
               </Grid>
               <Grid item xs={12}>
               </Grid>
             </Grid>
+            {(signUpError.length >= 1 
+                ? 
+                  <Typography variant='h7' color="error" gutterBottom>
+                    {bull} Error: {signUpError}
+                  </Typography>
+                : <></>
+            )}
             <Button
               type="submit"
               size='large'
@@ -155,7 +205,6 @@ export function UserSignUp() {
             >
               Sign Up
             </Button>
-            {err && <span>Soemthing went wrong</span>}
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/signin" variant="body2" underline='hover' sx={{ color: 'primary.light' }}>
