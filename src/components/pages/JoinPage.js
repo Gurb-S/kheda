c983cf1881f9d001efbd721269064fe80ed51f7d
gurb <Organicmintz@gmail.com>
2023-01-10 13:35:44 -0800

    create ui for join page

---
+++ src/components/pages/JoinPage.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PinInput from 'react-pin-input';
+
+//* Material UI
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+//* IMGS
+import logo from "../../imgs/dice.png";
+
+
+export function JoinPage(){
+    return(
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar alt="Kheda logo" src={logo} sx={{ width: 100, height: 100 }} />
+          <Typography component="h1" variant="h4">
+            Join Game
+          </Typography>
+          <Box component="form" noValidate sx={{ my: 10  }}>
+            <Grid container sx={{  justifyContent: 'center' }}>
+                <Typography component="h5" variant="div" sx={{ textAlign: 'center', mb: 3 }}>
+                    Enter Code
+                </Typography>
+            </Grid>
+            <Grid>
+                <PinInput 
+                    length={6} 
+                    initialValue=""
+                    onChange={(value, index) => {}} 
+                    type="numeric" 
+                    inputMode="number"
+                    style={{ padding: 'auto' }}  
+                    inputStyle={{border: 'none', borderRadius: '12px', backgroundColor: 'white', color: '#EE3B55'}}
+                    inputFocusStyle={{borderColor: 'none'}}
+                    onComplete={(value, index) => {}}
+                    autoSelect={false}
+                    regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+                />
+            </Grid>
+            <Button
+              type="submit"
+              size='large'
+              variant="contained"
+              sx={{ mt: 5, mb: 2, borderRadius: '16px', display: 'block', mx: 'auto' }}
+            >
+              Join
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    )
+}
