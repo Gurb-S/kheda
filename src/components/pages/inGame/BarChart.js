9fee0349a7be1bc3edf57fdf3eebb7943a392368
Gurb-S <OrganicMintz@gmail.com>
2023-01-14 00:58:35 -0800

    add better chart

--- src/components/pages/inGame/BarChart.js
+++ src/components/pages/inGame/BarChart.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Bar } from "react-chartjs-2"
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Data } from '../../../TestData'
+import { 
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend 
+} from 'chart.js/auto'
+
+import SiteContext from '../../../context/Context';
 
 //* Material ui
 import Box from '@mui/material/Box';
@@ -9,33 +18,113 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend 
+)
+
 export function BarChart() {
 
-    const [uiData, setUiData] = useState({
-        labels: Data.map((data) => data.year),
-        datasets: [{
-            label: 'X value?',
-            data: Data.map((data) =>data.userGain)
-        }]
-    })
+    const { currentUser } = useContext(SiteContext)
 
-    const config = {
-        data: uiData,
-        options: {
-            indexAxis: 'y'
-        }
+    // const [chartData, setChartData] = useState({
+    //     datasets: [],
+    // });
+
+    // const [chartOptions, setChartOptions] = useState({});
+
+
+    const testChartData = {
+        labels: [currentUser.displayName, "Kevin", "Geroge", "Micheal", "Oreo","Listen", "Post", "Kold"],
+        datasets: [
+          {
+            label: "Score",
+            data: [2, 1, 4, 2, 3, 5, 6],
+            borderColor: "#000000",
+            backgroundColor: "#EE3B55",
+          },
+        ],
     }
 
+    const testOptions ={
+        responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+            tooltip:{
+                enabled: false
+            },
+            legend: {
+              position: "top",
+            },
+            title: {
+              display: false,
+              text: "Whom'st let the dogs out",
+            },
+            customCanvasBackgroundColor: {
+                color: 'lightGreen',
+            }
+          },
+          indexAxis: 'y',
+          elements: {
+            bar: {
+              borderWidth: 2,
+            }
+          },
+    }
+        // setChartData({
+        //   labels: [currentUser.displayName, "Kevin", "Geroge", "Micheal", "Oreo","Listen", "Post", "Kold"],
+        //   datasets: [
+        //     {
+        //       label: "Score",
+        //       data: [2, 1, 4, 2, 3, 5, 6],
+        //       borderColor: "#000000",
+        //       backgroundColor: "#EE3B55",
+        //     },
+        //   ],
+        // });
+        // setChartOptions({
+        //   responsive: true,
+        //   maintainAspectRatio: true,
+        //   plugins: {
+        //     tooltip:{
+        //         enabled: false
+        //     },
+        //     legend: {
+        //       position: "top",
+        //     },
+        //     title: {
+        //       display: false,
+        //       text: "Whom'st let the dogs out",
+        //     },
+        //     customCanvasBackgroundColor: {
+        //         color: 'lightGreen',
+        //     }
+        //   },
+        //   indexAxis: 'y',
+        //   elements: {
+        //     bar: {
+        //       borderWidth: 2,
+        //     }
+        //   },
+        // });
+
     return (
-        <Box component="form" noValidate sx={{ mt: 0 }}>
-            <Typography id='modal-modal-title' gutterBottom variant="h3" component="h4" align='center' sx={{ mb: 4, fontSize: 20,textDecoration: 'underline', }} >
+        <div>
+            <Typography id='modal-modal-title' gutterBottom variant="h3" component="h4" align='center' sx={{ mb: 4, fontSize: 20,textDecoration: 'underline' }} >
                 Scores
             </Typography> 
-            <Card sx={{ maxWidth: 345, mx: 'auto', mt: 0 }}>
-                <CardContent sx={{ pt: 1 }}>
-                    <Bar data={uiData} />
-                </CardContent>
-            </Card> 
-        </Box>
+            <Bar data={testChartData} options={testOptions} />
+        </div>
+
+        // <Box component="form" noValidate sx={{ mt: 0 }}>
+        //     <Typography id='modal-modal-title' gutterBottom variant="h3" component="h4" align='center' sx={{ mb: 4, fontSize: 20,textDecoration: 'underline', }} >
+        //         Scores
+        //     </Typography> 
+        //     <Bar data={testChartData} options={testOptions} />
+        // </Box> 
     )
 }
